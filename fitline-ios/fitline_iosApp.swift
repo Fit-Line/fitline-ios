e0ba073feaf0831f6f79246f4cb24ebefff6ccfb
PleBea <bini060315kim@gmail.com>
2024-04-29 16:05:08 +0900

    Initial Commit

---
+++ fitline-ios/fitline_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitline_iosApp.swift
+//  fitline-ios
+//
+//  Created by 김성빈 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct fitline_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
