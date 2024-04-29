0861895bf2f461d14e134ee8d6581d779e00408c
PleBea <bini060315kim@gmail.com>
2024-04-30 02:05:31 +0900

    fix: typography, folder structure

--- fitline-ios/View/Screen/SplashScreen.swift
+++ fitline-ios/View/Screen/SplashScreen.swift
@@ -7,24 +7,26 @@
 
 import SwiftUI
 
-struct Screen: View {
+struct SplashScreen: View {
     @State private var isActive = false
     
     var body: some View {
         VStack {
-            VStack{
-                HStack{
+            VStack(spacing: 8) {
+                HStack(spacing: 20) {
                     Image("Logo")
                         .resizable()
                         .frame(width: 60,height: 60)
                     
+                    CustomText(text: "FitLine", color: .Colors.Grayscale._90, fontType: .Display)
                 }
                 
+                CustomText(text: "체형 맞춤 건강 관리 앱", color: .Colors.Grayscale._50, fontType: .Body)
             }
         }
     }
 }
 
 #Preview {
-    Screen()
+    SplashScreen()
 }
---
+++ fitline-ios/View/Shared/Typography.swift
@@ -0,0 +1,23 @@
+//
+//  Typography.swift
+//  fitline-ios
+//
+//  Created by 김성빈 on 4/29/24.
+//
+
+import SwiftUI
+
+struct CustomText: View {
+    let text: String
+    let color: Color
+    let fontType: FontType
+    
+    var body: some View {
+        let fontSetting = fontType.setting
+        Text(text)
+            .font(.custom(fontSetting.name, size: fontSetting.size))
+            .tracking((fontSetting.size * fontSetting.tracking) / 100)
+            .kerning(fontSetting.kerning)
+            .foregroundStyle(color)
+    }
+}
