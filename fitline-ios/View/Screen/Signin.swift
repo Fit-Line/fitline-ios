e9f71de5aa5c61b8960e1a0f613d37113b8969ad
PleBea <bini060315kim@gmail.com>
2024-05-10 10:30:02 +0900

    fix: signin page reference

--- fitline-ios/View/Screen/Signin.swift
+++ fitline-ios/View/Screen/Signin.swift
@@ -9,37 +9,41 @@ import SwiftUI
 
 struct Signin: View {
     var body: some View {
-        Spacer()
-        VStack {
-            VStack(spacing: 8) {
-                HStack(spacing: 20) {
-                    Image("Logo")
-                        .resizable()
-                        .frame(width: 60, height: 60)
-
-                    CustomText(text: "FitLine", color: .Colors.Grayscale._90, fontType: .Display)
+        ZStack {
+            VStack {
+                VStack(spacing: 8) {
+                    HStack(spacing: 20) {
+                        Image("Logo")
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        
+                        CustomText(text: "FitLine", color: .Colors.Grayscale._90, fontType: .Display)
+                    }
+                    
+                    CustomText(text: "체형 맞춤 건강 관리 앱", color: .Colors.Grayscale._50, fontType: .Body)
                 }
-
-                CustomText(text: "체형 맞춤 건강 관리 앱", color: .Colors.Grayscale._50, fontType: .Body)
+            }
+            VStack {
+                Spacer()
+                
+                Button(action: /*@START_MENU_TOKEN@*/ {}/*@END_MENU_TOKEN@*/, label: {
+                    HStack(spacing: 10){
+                        Image("GoogleLogo")
+                            .resizable()
+                            .frame(width: 18, height: 18)
+                        CustomText(text: "구글 계정으로 로그인", color: .Colors.Grayscale._10, fontType: .BodyStrong)
+                    }
+                    .cornerRadius(12)
+                    .foregroundColor(Color.Colors.Grayscale._10)
+                    .frame(width: .infinity, height: 58)
+                    
+                }).frame(maxWidth: .infinity)
+                    .padding(.horizontal, 20)
+                    .background(Color.Colors.Grayscale._90)
+                    .cornerRadius(12)
+                    .padding(.horizontal, 20)
             }
         }
-        Spacer()
-        Button(action: /*@START_MENU_TOKEN@*/ {}/*@END_MENU_TOKEN@*/, label: {
-            HStack(spacing: 10){
-                Image("GoogleLogo")
-                    .resizable()
-                    .frame(width: 18, height: 18)
-                CustomText(text: "구글 계정으로 로그인", color: .Colors.Grayscale._10, fontType: .BodyStrong)
-            }
-                .cornerRadius(12)
-                .foregroundColor(Color.Colors.Grayscale._10)
-                .frame(width: .infinity, height: 58)
-
-        }).frame(maxWidth: .infinity)
-            .padding(.horizontal, 20)
-            .background(Color.Colors.Grayscale._90)
-            .cornerRadius(12)
-            .padding(.horizontal, 20)
 
     }
 }
